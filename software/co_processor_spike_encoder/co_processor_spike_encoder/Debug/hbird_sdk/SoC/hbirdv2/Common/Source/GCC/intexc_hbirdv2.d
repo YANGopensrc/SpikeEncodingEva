c80055a0de62a1410d81c67626aa21f7b736944e
YANGopensrc <yongkuiyang@hotmail.com>
2026-03-26 20:03:44 +0800

    Add files via upload

---
+++ software/co_processor_spike_encoder/co_processor_spike_encoder/Debug/hbird_sdk/SoC/hbirdv2/Common/Source/GCC/intexc_hbirdv2.d
@@ -0,0 +1,8 @@
+hbird_sdk/SoC/hbirdv2/Common/Source/GCC/intexc_hbirdv2.o: \
+ ../hbird_sdk/SoC/hbirdv2/Common/Source/GCC/intexc_hbirdv2.S \
+ D:\NucleiStudio_IDE_202102-win64\NucleiStudio_workspace\co_processor_spike_encoder\co_processor_spike_encoder\hbird_sdk\NMSIS\Core\Include/riscv_encoding.h \
+ D:\NucleiStudio_IDE_202102-win64\NucleiStudio_workspace\co_processor_spike_encoder\co_processor_spike_encoder\hbird_sdk\NMSIS\Core\Include/riscv_bits.h
+
+D:\NucleiStudio_IDE_202102-win64\NucleiStudio_workspace\co_processor_spike_encoder\co_processor_spike_encoder\hbird_sdk\NMSIS\Core\Include/riscv_encoding.h:
+
+D:\NucleiStudio_IDE_202102-win64\NucleiStudio_workspace\co_processor_spike_encoder\co_processor_spike_encoder\hbird_sdk\NMSIS\Core\Include/riscv_bits.h:

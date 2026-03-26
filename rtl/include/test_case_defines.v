5424058c6cc12e01fb57f6ccf22e042a19fb9f7d
YANGopensrc <yongkuiyang@hotmail.com>
2026-03-26 20:07:36 +0800

    Add files via upload

---
+++ rtl/include/test_case_defines.v
@@ -0,0 +1,15 @@
+//-----Please select one of the test cases------//
+
+`define MNIST_ISI_ENCODE_TEST                     //MNIST dataset which use isi_spike encode
+//`define MNIST_POISS_ENCODE_TEST                 //MNIST dataset which use poiss_spike encode
+//`define MNIST_TTFS_GUISS_DIFFER_ENCODE_TEST     //MNIST dataset which use ttfs_spike encode based on guiss_differ result
+//`define MNIST_TTFS_SRC_ENCODE_TEST              //MNIST dataset which use ttfs_spike encode based on source image
+//`define CIFAR10_ISI_ENCODE_TEST                 //CIFAR10 dataset which use isi_spike encode
+//`define CIFAR10_POISS_ENCODE_TEST               //CIFAR10 dataset which use poiss_spike encode
+//`define CIFAR10_TTFS_GUISS_DIFFER_TEST          //CIFAR10 dataset which use ttfs_spike encode based on guiss_differ result
+//`define CIFAR10_TTFS_SRC_ENCODE_TEST            //CIFAR10 dataset which use ttfs_spike encode based on source image
+//`define IMAGENET_ISI_ENCODE_TEST                //IMAGENET dataset which use isi_spike encode
+//`define IMAGENET_POISS_ENCODE_TEST              //IMAGENET dataset which use poiss_spike encode
+//`define IMAGENET_TTFS_GUISS_DIFFER_TEST         //IMAGENET dataset which use ttfs_spike encode based on guiss_differ result
+//`define IMAGENET_TTFS_SRC_TEST                  //IMAGENET dataset which use ttfs_spike encode based on source image
+
